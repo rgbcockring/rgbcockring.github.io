22a7f29a4995eb9a4ce643712c302487c7b32497
rgbcockring <80497857+rgbcockring@users.noreply.github.com>
2023-10-12 02:24:05 -0600

    Update age_timer.js

--- age_timer.js
+++ age_timer.js
@@ -11,13 +11,13 @@ function startTimer() {
         return;
     }
 
-   
+  
     titleElement.textContent = "Age Timer";
 
-    
+   
     titleElement.style.display = "none";
     ageLabelElement.style.display = "none";
-    birthdateInput.style.display = "none";
+    birthdateInput.style display = "none";
     startButton.style.display = "none";
 
     let ageYears = 0;
@@ -26,14 +26,14 @@ function startTimer() {
         const now = new Date();
         const ageMilliseconds = now - birthdate;
 
-      
+       
         ageYears = ageMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
 
-       
-        timerElement.innerHTML = `<span class="gradient-text">${ageYears.toFixed(12)}</span>`;
+        
+        timerElement.innerHTML = `<span class="gradient-text">${ageYears.toFixed(12).substring(0, 13)}</span>`;
     }
 
-    
+  
     timerElement.style.display = "block";
 
     updateTimer();
